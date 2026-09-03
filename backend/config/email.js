dc3f1fb9e3490c7270c1c67427c6dcb4940469ed
StarBlaiz <sundayhezekiah203@gmail.com>
2026-09-03 13:08:45 +0100

    Fix password reset email SMTP connection

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
 
   auth: {
     user: process.env.EMAIL_USER,
@@ -9,13 +12,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
 const sendEmail = async ({
   to,
   subject,
   text,
   html,
 }) => {
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
+    throw new Error(
+      "EMAIL_USER or EMAIL_PASSWORD is missing from environment variables."
+    );
+  }
 
   await transporter.sendMail({
     from: `"Blaiz Business Manager" <${process.env.EMAIL_USER}>`,
@@ -24,8 +31,6 @@ const sendEmail = async ({
     text,
     html,
   });
-
 };
-
 
 module.exports = sendEmail;
